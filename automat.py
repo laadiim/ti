fa008ace90d0be5675a400d98382e97bc96b3ec3
laadiim <cakora.ladislav@gmail.com>
2024-12-16 12:19:31 +0100

    automat.py - basic

---
+++ automat.py
@@ -0,0 +1,29 @@
+class Automat:
+
+    def __init__(self, matrix):
+        self.__matrix = matrix
+        self.__inputs = {
+                            "a": 0, #odpovida prvnimu poli v matici: "[[a], []]"
+                            "b": 1  #odpovida druhemu poli v matici: "[[], [b]]"
+                        }  
+        self.current = 0
+
+    def move(self, inp: str) -> int:
+        index = self.__inputs[inp]
+        self.current = self.__matrix[index][self.current]
+        return self.current
+
+
+def main():
+    matrix1 = [[0, 0, 0, 3], [1, 2, 3, 3]]
+    automat1 = Automat(matrix1)
+
+    matrix2 = [[0, 0, 3, 0, 4], [1, 2, 2, 4, 4]]
+    automat2 = Automat(matrix2)
+
+    matrix3 = [[0, 0, 4, 3, 0, 7, 7, 7], [1, 2, 3, 3, 5, 6, 3, 5]]
+    automat3 = Automat(matrix3)
+
+
+if __name__ == "__main__":
+    main()
